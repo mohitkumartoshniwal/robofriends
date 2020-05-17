03a08856faae1b5cccefb199844f1782a953c57a
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2020-05-17 21:11:24 +0530

    created action, reducer for searchfield as well as created store and used provider component

--- src/index.js
+++ src/index.js
@@ -10,5 +10,8 @@ import { searchRobots } from './reducers';
 
 const store=createStore(searchRobots)
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+    <App  />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
